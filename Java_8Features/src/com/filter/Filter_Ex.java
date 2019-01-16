758d90e50b48a7f1736c1addc63ff9f814d1bd7c
aman.bhavsar <aman.bhavsar@KA-LPT-052.kadc.com>
2019-01-16 10:23:56 +0530

    path name

--- Java_8Features/src/com/filter/Filter_Ex.java
+++ Java_8Features/src/com/filter/Filter_Ex.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class Filter_Ex {
 	public static void main(String[] args) {
 		Map<Integer,String> map=new HashMap<>();
-		map.put(1, "Bhavin");
+		map.put(1, "parth");
 		map.put(2, "Dhruv");
 		String result="";
 		 result=map.entrySet().stream()
